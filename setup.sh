1edbf876b8c058db98026feebd3b991cc9dc1fee
Sam Weitzman <sam@aquabyte.ai>
2022-05-07 03:43:04 +0000

    remove vim setup and unsupported function

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 function safely_point_to_new_dotfile {
   local BASENAME=$1
   local DOTFILE=$HOME/$BASENAME
-  local CHECKED_IN_DOTFILE=$HOME/dotfiles/$BASENAME
+  local CHECKED_IN_DOTFILE=$SCRIPT_DIR/$BASENAME
 
   # backup old dotfile
   if test -f "$DOTFILE"; then
@@ -23,21 +25,6 @@ function setup_preferred_hostname {
   echo "export PREFERRED_HOSTNAME=$hostname" >> $HOME/.bashrc.local
 }
 
-function setup_preferred_color {
-  echo What base color would you like to use for bash prompt and tmux?
-  echo 1. Red
-  echo 2. Blue
-  echo 3. Purple
-  echo 4. Cyan
-  echo 5. Magenta
-  echo 6. Green
-  echo 7. Yellow
-  echo 8. White
-  read color
-
-  echo Sorry this functionality is not supported yet.
-}
-
 echo "=== Creating empty local versions of dotfiles and backing up old ones"
 safely_point_to_new_dotfile .bash_login
 safely_point_to_new_dotfile .bash_profile
@@ -47,19 +34,9 @@ safely_point_to_new_dotfile .inputrc
 safely_point_to_new_dotfile .bash_logout
 
 setup_preferred_hostname
-setup_preferred_color
 
 echo "=== Setting up tmux"
 safely_point_to_new_dotfile .tmux.conf
 git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
-
-echo "=== Setting up vim"
-ln -sv $HOME/dotfiles/.vim $HOME
-touch $HOME/.vim/vimrc.local
-./.vim/vim_setup.sh
-
-echo "=== Setting up emacs"
-ln -sv $HOME/dotfiles/.emacs.d $HOME
-cp $HOME/dotfiles/.emacs.d/default_local_configuration.org $HOME/.emacs.local.org
 
 echo "=== Setup complete"
